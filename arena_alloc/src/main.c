b2431fdeb98599700b001cd92eb4487446c68d8d
Argyn Zhienbay <argynzhienbay@MacBook-Pro-Argyn.local>
2026-01-27 02:00:05 +0500

    [arena_malloc] init

---
+++ arena_alloc/src/main.c
@@ -0,0 +1,78 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
+typedef int64_t i64;
+typedef int32_t i32;
+typedef int16_t i16;
+typedef int8_t i8;
+
+typedef i8 b8;
+typedef i32 b32;
+
+#define KiB(n) ((u64)(n) << 10)
+#define MiB(n) ((u64)(n) << 20)
+#define GiB(n) ((u64)(n) << 30)
+
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#define ALIGN_UP_POW2(n, p) (((u64)(n)) + ((u64)(p) - 1)) & (~((u64)(p) - 1))
+
+#define ARENA_BASE_POS (sizeof(mem_arena))
+#define ARENA_ALIGN (sizeof(void *))
+
+typedef struct {
+  u64 capacity;
+  u64 pos;
+} mem_arena;
+
+mem_arena *arena_create(u64 capcity);
+void arena_destroy(mem_arena *arena);
+
+void *arena_push(mem_arena *arena, u64 size, b32 non_zero);
+void arena_pop(mem_arena *arena, u64 size);
+void arena_pop_to(mem_arena *arena, u64 pos);
+void arena_clear(mem_arena *arena);
+
+int main(void) { return EXIT_SUCCESS; }
+
+mem_arena *arena_create(u64 capcity) {
+  mem_arena *arena = (mem_arena *)malloc(capcity);
+
+  arena->capacity = capcity;
+  arena->pos = ARENA_BASE_POS;
+
+  return arena;
+}
+
+void arena_destroy(mem_arena *arena) { free(arena); }
+
+void *arena_push(mem_arena *arena, u64 size, b32 non_zero) {
+  u64 pos_aligned = ALIGN_UP_POW2(arena->pos, ARENA_ALIGN);
+  u64 new_pos = pos_aligned + size;
+
+  if (new_pos > arena->capacity) {
+    return NULL;
+  }
+
+  arena->pos = new_pos;
+
+  u8 *out = (u8 *)arena + pos_aligned;
+
+  if (!non_zero) {
+    memset(out, 0, size);
+  }
+
+  return out;
+}
+
+void arena_pop(mem_arena *arena, u64 size);
+
+void arena_pop_to(mem_arena *arena, u64 pos);
+
+void arena_clear(mem_arena *arena);
